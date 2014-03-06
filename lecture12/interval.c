133cf4802593f0de41418f11d01275ae02c3b9d5
alisonberkowitz <alison.berkowitz@students.olin.edu>
2014-03-06 11:27:02 -0500

    fixing interval

--- lecture12/interval.c
+++ lecture12/interval.c
@@ -38,19 +38,26 @@ typedef struct {
 // Returns a new Interval with the given start and end times.
 // If unable to allocate, prints an error message and exits.
 Interval *make_interval(time_t start, time_t end) {
-    // fill this in
-    return NULL;
+    Interval *i = (Interval*)malloc(sizeof(Interval));
+    if (i == NULL) {
+        fprintf(stderr, "failed to allocate memory to new interval.\n");
+        exit(-1);
+    }
+    i->start = start;
+    i->end = end;
+    return i;
 }
 
 // Computes the duration of an Interval using difftime.
 double interval_duration(Interval *interval) {
-    // fill this in
-    return 0.0;
+    double diff_t;
+    diff_t = difftime(interval->end, interval->start);
+    return diff_t;
 }
 
 // Frees an Interval.
 void free_interval(Interval *interval) {
-    // fill this in
+    free(interval);
 }
 
 // Prints an interval in local time.
